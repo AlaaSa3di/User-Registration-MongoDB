02a3fa70f3788163c90cf0fcf1fee7ce5d21c95d
AlaaSa3di <alaa.saadi95@gmail.com>
2025-03-11 00:21:06 +0300

    Add new Documemts

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const connectDB = require("./config/db");
+
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => console.log("Connected to MongoDB 🥭"))
-    .catch(err => console.error("MongoDB connection error:", err));
-
+connectDB();
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
